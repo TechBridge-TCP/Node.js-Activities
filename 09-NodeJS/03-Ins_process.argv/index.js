da02515bdb155d912b91b6ff0b81e868c4fc2245
Dannie Digital <danielle.beaty@codesistas.co>
2020-07-22 16:26:46 -0400

    Updated index.js

--- 09-NodeJS/03-Ins_process.argv/index.js
+++ 09-NodeJS/03-Ins_process.argv/index.js
@@ -3,3 +3,21 @@ console.log(process.argv);
 
 // arguments passed from the command line are accessed by index
 // console.log(process.argv[2]);
+// returns an array of command line arguments
+console.log(process.argv);
+
+// arguments passed from the command line are accessed by index
+console.log(process.argv[1]);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+var firstName = process.argv[2];
+var lastName = process.argv[3];
+
+
+
+if (firstName === undefined) {
+    console.log(`First name is required...`)
+}
+
+console.log (`My name is ${firstName} ${lastName}`);
